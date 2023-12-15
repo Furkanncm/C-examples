e8c4aef1069533deea86c0481c949a9d536bd5d5
Furkan ÇAM <120260143+Furkanncm@users.noreply.github.com>
2023-12-15 22:32:14 +0300

    BresenhamAlgorithm

---
+++ Bresenham2/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Bresenham2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.pictureBox1.Location = new System.Drawing.Point(38, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(722, 370);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox1_Paint);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(323, 389);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 31);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Alanı Temizle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Bresenham2/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bresenham2
+{
+    public partial class Form1 : Form
+    {
+        private List<Point> linePoints = new List<Point>();
+        private int pixelSize = 5; // Her pikselin boyutu
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void PictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawBresenhamLine(e.Graphics);
+        }
+        private void DrawBresenhamLine(Graphics g)
+        {
+            if (linePoints.Count >= 2)
+            {
+                Point startPoint = linePoints[0];
+                Point endPoint = linePoints[1];
+
+                int x1 = startPoint.X;
+                int y1 = startPoint.Y;
+                int x2 = endPoint.X;
+                int y2 = endPoint.Y;
+
+                int dx = Math.Abs(x2 - x1);
+                int dy = Math.Abs(y2 - y1);
+
+                int sx = (x1 < x2) ? 1 : -1;
+                int sy = (y1 < y2) ? 1 : -1;
+                int err = dx - dy;
+
+                while (true)
+                {
+                    DrawPixel(g, x1, y1);
+
+                    if (x1 == x2 && y1 == y2)
+                        break;
+
+                    int e2 = 2 * err;
+
+                    if (e2 > -dy)
+                    {
+                        err -= dy;
+                        x1 += sx;
+                    }
+
+                    if (e2 < dx)
+                    {
+                        err += dx;
+                        y1 += sy;
+                    }
+                }
+            }
+        }
+        private void DrawPixel(Graphics g, int x, int y)
+        {
+            g.FillRectangle(Brushes.Black, x * pixelSize, y * pixelSize, pixelSize, pixelSize);
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (linePoints.Count < 2)
+            {
+                linePoints.Add(new Point(e.X / pixelSize, e.Y / pixelSize));
+                pictureBox1.Refresh();
+            }
+            else
+            {
+                MessageBox.Show("İki noktadan fazlasını ekleyemezsiniz.");
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            linePoints.Clear();
+            pictureBox1.Refresh();
+        }
+    }
+}
