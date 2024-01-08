ec385226a13287a2936e4937305f3fdcf078806a
Furkan ÇAM <120260143+Furkanncm@users.noreply.github.com>
2024-01-08 11:50:21 +0300

    Final

---
+++ Final/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Final
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.once = new System.Windows.Forms.Button();
+            this.sonra = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ara = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(50, 1);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(828, 200);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // once
+            // 
+            this.once.Location = new System.Drawing.Point(400, 800);
+            this.once.Name = "once";
+            this.once.Size = new System.Drawing.Size(75, 23);
+            this.once.TabIndex = 4;
+            this.once.Text = "Önceki";
+            this.once.UseVisualStyleBackColor = true;
+            this.once.Click += new System.EventHandler(this.once_Click);
+            // 
+            // sonra
+            // 
+            this.sonra.Location = new System.Drawing.Point(500, 800);
+            this.sonra.Name = "sonra";
+            this.sonra.Size = new System.Drawing.Size(75, 23);
+            this.sonra.TabIndex = 5;
+            this.sonra.Text = "Sonraki";
+            this.sonra.UseVisualStyleBackColor = true;
+            this.sonra.Click += new System.EventHandler(this.sonra_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(409, 271);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(333, 252);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Visible = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(211, 62);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(88, 35);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Ekle";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Saat";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(17, 166);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(130, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Notunuz";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(211, 166);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 35);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Çıkış";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(211, 113);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 35);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Güncelle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(17, 86);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(130, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(654, 788);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Sil";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ara
+            // 
+            this.ara.Location = new System.Drawing.Point(252, 800);
+            this.ara.Name = "ara";
+            this.ara.Size = new System.Drawing.Size(75, 23);
+            this.ara.TabIndex = 7;
+            this.ara.Text = "Ara";
+            this.ara.UseVisualStyleBackColor = true;
+            this.ara.Click += new System.EventHandler(this.ara_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(89, 800);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(134, 22);
+            this.textBox3.TabIndex = 8;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(784, 756);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(214, 132);
+            this.listBox1.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1664, 956);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.ara);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.sonra);
+            this.Controls.Add(this.once);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Takvim Uygulaması";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Button once;
+        private Button sonra;
+        private GroupBox groupBox1;
+        private Button button2;
+        private TextBox textBox1;
+        private Label label1;
+        private Button button4;
+        private Button button3;
+        private Label label2;
+        private TextBox textBox2;
+        private Button button5;
+        private Button ara;
+        private TextBox textBox3;
+        private ListBox listBox1;
+    }
+}
+
---
+++ Final/Form1.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Final.Form1;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+//21360859036 FURKAN KAZIM ÇAM
+
+namespace Final
+{
+    public partial class Form1 : Form
+    {
+        private int suAnkiSayfa = 0;
+        private int toplamSutun = 395;
+        private int SayfadakiSutun = 8;
+        public class Note
+        {
+            public string Day { get; set; }
+            public int StartHour { get; set; }
+            public int Duration { get; set; }
+            public string Text { get; set; }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCalendar();
+            SutunGorunurluk();
+            SayfayıGuncelle();
+            this.Load += AnaForm_Load;
+            this.Resize += AnaForm_Resize;
+
+        }
+        private void AnaForm_Resize(object sender, EventArgs e)
+        {
+            // DataGridView'in boyutunu form boyutuna uyacak şekilde güncelle
+            dataGridView1.Size = new System.Drawing.Size(this.ClientSize.Width - 50, this.ClientSize.Height - 50);
+        }
+        private void AnaForm_Load(object sender, EventArgs e)
+        {
+            this.StartPosition = FormStartPosition.Manual;
+            this.Left = Screen.PrimaryScreen.WorkingArea.Width / 2;
+            this.Top = 0;
+            this.Width = Screen.PrimaryScreen.WorkingArea.Width / 2;
+            this.Height = Screen.PrimaryScreen.WorkingArea.Height;
+        }
+
+
+        private void InitializeCalendar()
+        {
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.AllowUserToAddRows = false;
+
+            // Haftalık ve saatlik gösterim için sütunlar eklenir
+            GunYaz();
+            SaatYaz();
+            // DataGridView form üzerine eklenir
+            this.Controls.Add(dataGridView1);
+        }
+        private void GunYaz()
+        {
+            // Haftalık sütunlar eklenir
+            DateTime date = DateTime.Now;
+
+            dataGridView1.Columns.Add("Time", "Zaman");
+            dataGridView1.Columns.Add("Monday", date.ToString("dd/MM/yy/dddd"));
+            for (int i = 0; i < 395; i++)
+            {
+                date = date.AddDays(1);
+                dataGridView1.Columns.Add("Tuesday", date.ToString("dd/MM/yy/dddd"));
+            }
+
+            dataGridView1.Columns[0].ReadOnly = true; // Saat sütunu salt okunur olmalıdır
+            // ekranda gösterilecek sütun sayısı ayarlanır
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.AllowUserToAddRows = false;
+
+
+        }
+
+        private void SaatYaz()
+        {
+            // Saatlik satırlar eklenir
+            for (int hour = 0; hour < 24; hour++)
+            {
+                dataGridView1.Rows.Add($"{hour}:00 ");
+            }
+            dataGridView1.Rows[0].ReadOnly = true; // Günlerin olduğu satır salt okunur olmalıdır
+
+        }
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            groupBox1.Visible = true;
+            if (dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[dataGridView1.CurrentCell.ColumnIndex].Style.BackColor == Color.Red)
+            {
+                groupBox1.Visible=false;
+                MessageBox.Show("Seçili hücreye not eklenemez");
+
+            }
+
+        }
+        private void SutunGorunurluk()
+        {
+            // Görüntülenecek sütunları belirle
+            for (int i = 0; i < dataGridView1.Columns.Count; i++)
+            {
+                if (i == 0)
+                {
+                    // "Saat" sütunu her zaman görünür
+                    dataGridView1.Columns[i].Visible = true;
+                }
+                else
+                {
+                    // Diğer sütunları belirle
+                    dataGridView1.Columns[i].Visible = ((i - 1) >= suAnkiSayfa * (SayfadakiSutun -1) && (i - 1) < (suAnkiSayfa + 1) * (SayfadakiSutun - 1));
+                }
+            }
+        }
+        private void SayfayıGuncelle()
+        {
+            // Önceki ve sonraki butonların durumunu güncelle
+            once.Enabled = suAnkiSayfa > 0;
+            sonra.Enabled = (suAnkiSayfa + 1) * SayfadakiSutun < toplamSutun;
+        }
+
+
+        private void once_Click(object sender, EventArgs e)
+        {
+            if (suAnkiSayfa > 0)
+            {
+                suAnkiSayfa--;
+                SutunGorunurluk();
+                SayfayıGuncelle();
+            }
+
+        }
+
+        private void sonra_Click(object sender, EventArgs e)
+        {
+            if ((suAnkiSayfa + 1) * SayfadakiSutun < toplamSutun)
+            {
+                suAnkiSayfa++;
+                SutunGorunurluk();
+                SayfayıGuncelle();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentCell.ColumnIndex != 0&& dataGridView1.CurrentCell.RowIndex!=0)
+            {
+                dataGridView1.CurrentCell.Value = "";
+                dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[dataGridView1.CurrentCell.ColumnIndex].Style.BackColor=Color.White;
+            }
+
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            groupBox1.Visible = false;
+
+
+        }
+
+        string[] notes = new string[100];
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+
+            //seçili olan hücreye texti yazdırır
+            if (dataGridView1.CurrentCell.ColumnIndex != 0 && dataGridView1.CurrentCell.RowIndex != 0)
+            {
+                dataGridView1.CurrentCell.Value = textBox1.Text;
+                //textboxa eklenen texti diziye kaydet
+                for (int i = 0; i < notes.Length; i++)
+                {
+                    notes[i] = textBox1.Text;
+                }
+
+                dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[dataGridView1.CurrentCell.ColumnIndex].Style.BackColor=Color.Yellow;
+                //girilen saat kadar hücreyi boyar
+                for (int i = 1; i < Convert.ToInt32(textBox2.Text); i++)
+                {
+                    dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex + i].Cells[dataGridView1.CurrentCell.ColumnIndex].Style.BackColor = Color.Red;
+
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Seçili hücreye not eklenemez");
+
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //hücre boş değilse hücreyi günceller
+
+
+            if (dataGridView1.CurrentCell.Value==null)
+            {
+                MessageBox.Show("Boş olan hücre güncellenemez");
+            }
+
+            if (dataGridView1.CurrentCell.ColumnIndex != 0 && dataGridView1.CurrentCell.RowIndex != 0)
+            {
+                dataGridView1.CurrentCell.Value = textBox1.Text;
+            }
+            else
+            {
+                MessageBox.Show("Seçili hücre güncellenemez");
+
+            }
+            dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex].Cells[dataGridView1.CurrentCell.ColumnIndex].Style.BackColor=Color.Yellow;
+            //girilen saat kadar hücreyi boyar
+            for (int i = 1; i < Convert.ToInt32(textBox2.Text); i++)
+            {
+                dataGridView1.Rows[dataGridView1.CurrentCell.RowIndex + i].Cells[dataGridView1.CurrentCell.ColumnIndex].Style.BackColor = Color.Red;
+
+            }
+
+
+        }
+
+        private void ara_Click(object sender, EventArgs e)
+        {
+            string searchText = textBox3.Text.ToLower();
+
+            if (!string.IsNullOrEmpty(searchText)) // TextBox boş değilse işleme devam et
+            {
+                // textboxta yazılan değeri not dizisi içinde ara
+                for (int i = 0; i < notes.Length; i++)
+                {
+                    if (notes[i] == searchText)
+                    {
+                        // bulunan değeri listboxta göster
+                        if (listBox1.Items.Contains(searchText))
+                        {
+
+                            listBox1.Items.Add(searchText);
+                        }
+
+                        MessageBox.Show("Aranan değer bulundu");
+
+                        break;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Aranan değer bulunamadı.");
+
+                    }
+                }
+            }
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            string searchText = textBox3.Text.ToLower();
+            listBox1.Items.Clear(); // ListBox'ı temizle
+
+            if (!string.IsNullOrEmpty(searchText)) // TextBox boş değilse işleme devam et
+            {
+                bool found = false;
+
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    foreach (DataGridViewCell cell in row.Cells)
+                    {
+                        if (cell.Value != null && cell.Value.ToString().ToLower().Contains(searchText))
+                        {
+                            // Bulunan değeri ListBox'a ekle
+                            listBox1.Items.Add($"Row: {cell.RowIndex + 1}, Column: {cell.ColumnIndex + 1}, Value: {cell.Value}");
+                            found = true;
+                            break; // İlk bulunan değeri gösterdikten sonra döngüden çık
+                        }
+                    }
+                }
+
+                if (!found)
+                {
+                    listBox1.Items.Add("Aranan değer bulunamadı.");
+                }
+            }
+            else
+            {
+                // TextBox boşsa, ListBox'a bir mesaj ekle
+                listBox1.Items.Add("Arama metni girilmelidir.");
+            }
+        }
+    }
+}
+
